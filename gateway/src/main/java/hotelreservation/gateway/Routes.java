f60a5a345180ace1e8786548a1edf8c5f7b06484
Savina Daria <savina.dariia@chnu.edu.ua>
2024-11-20 00:32:50 +0200

    [4] api gateway

---
+++ gateway/src/main/java/hotelreservation/gateway/Routes.java
@@ -0,0 +1,33 @@
+package hotelreservation.gateway;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+@Configuration(proxyBeanMethods = false)
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> customerServiceRoute() {
+        return route("customer_service")
+                .route(RequestPredicates.path("/api/customer"), http("http://localhost:8082"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> reservationServiceRoute() {
+        return route("reservation_service")
+                .route(RequestPredicates.path("/api/reservation"), http("http://localhost:8083"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> roomServiceRoute() {
+        return route("room_service")
+                .route(RequestPredicates.path("/api/room"), http("http://localhost:8081"))
+                .build();
+    }
+}
