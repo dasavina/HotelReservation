f5c217a948a3acd7e319809ee3d79b239b2a9efb
Savina Daria <savina.dariia@chnu.edu.ua>
2024-11-19 23:30:13 +0200

    init for reservations service

---
+++ Reservations/src/test/java/hotelreservation/reservations/TestReservationsApplication.java
@@ -0,0 +1,11 @@
+package hotelreservation.reservations;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ReservationsApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
