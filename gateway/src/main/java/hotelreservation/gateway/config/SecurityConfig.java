87570ae38b9710fa91bf1da242ea2f840c984dfa
Savina Daria <savina.dariia@chnu.edu.ua>
2024-11-22 01:40:25 +0200

    debugging 1

--- gateway/src/main/java/hotelreservation/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/hotelreservation/gateway/config/SecurityConfig.java
@@ -1,12 +1,11 @@
-package hotelreservation.customers.config;
+package hotelreservation.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
